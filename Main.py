807dc27f10dc105e69795c55a9e6619aa2e6c6b7
James Mariotti <James.Mariotti.Lapointe@gmail.com>
2018-12-09 00:15:46 -0500

    WIP

---
+++ Main.py
@@ -0,0 +1,44 @@
+#COEN 352 Project
+def spawn (parent1,parent2):
+    #2nd parent in case of cross over
+    return
+
+def cull (poolOfContenders):
+    # reduce number of individuals to specified population
+    return
+
+class Mutate (parent):
+    #different mutation functions
+    def add (self):
+        #add new swap
+        return
+    def delete (self):
+        #
+        return
+    def modifiy (self):
+        #
+        return
+    
+    #Time permitting
+    #def swap (self):
+        ## swap
+        #return
+    
+class SortIndividual:
+    def __init__(self):
+        #member Variables
+        #TODO swaps
+        #TODO mutation history
+        #TODO fitness : effi and effe
+        #TODO age
+        return
+    #member Methods
+    #TODO swaps
+    #TODO mutation history
+    #TODO fitness measurement: effi 
+    #TODO fitness measurement: effe
+    #TODO increment age    
+    def spawn (self,parent2):
+            #generates copy of iteself (child)
+            #2nd parent in case of cross over
+        return
