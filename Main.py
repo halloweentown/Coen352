042cdb50fba6551411ab1f6cfea6364458a5f7d0
James Mariotti <James.Mariotti.Lapointe@gmail.com>
2018-12-12 15:39:14 -0500

    Add() works
    - removed Mutate class
    - mutate methods now belong to SortIndividual

--- Main.py
+++ Main.py
@@ -5,16 +5,17 @@ import random as rng
 import SortIndividual
 import globalVariables
 import countInversions
-import Mutate as mu
 
 # Input array to be sorted
 inputArray = [9,3,13,19,13,22,22,62,66,73,79,82,96,99,115,127,130,134,143,171,182,189,189,213,225,227,230,242,244,246,253,288,297,307,329,340,374,382,391,409,422,424,434,440,442,448,458,471,477,498,511,521,528,533,549,551,562,573,581,581,585,586,586,587,588,598,612,643,651,651,685,699,699,708,711,745,760,764,766,777,779,785,786,804,812,813,829,830,843,871,876,910,915,917,918,928,940,977,988,991]
 
 # Create initial sort to be evolved
 a=SortIndividual.SortIndividual()
+print(a.sort)
 
 # Mutate Offspring of individual
-Mutate.Mutate.add
+a.add()
+print(a.sort)
 
 # Tournament selection of best sort
 
@@ -23,11 +24,11 @@ Mutate.Mutate.add
 # End condition (# of iterations, and/or fitness criteria)
 
 
-_,inversions=countInversions.countInversions(inputArray)
+'''_,inversions=countInversions.countInversions(inputArray)
 print(sorted)
 sorted,_=countInversions.countInversions(inputArray)
 print(inversions)
-print(countInversions.countInversions(inputArray))
+print(countInversions.countInversions(inputArray))'''
 
 # Reference code for raw input
 # username = raw_input("What is your login? :  ")
--- SortIndividual.py
+++ SortIndividual.py
@@ -2,6 +2,11 @@ import Mutate
 import numbers as np
 import random as rng
 import globalVariables as gv
+#below from mutate class merge
+import numpy as np
+import random as rng
+import globalVariables as gV
+import SortIndividual
 
 #inputArray = [1,2,3,4]
 #power = 3
@@ -13,23 +18,23 @@ class SortIndividual:
     def __init__(self):
         # ~~~~~~~~~~~~~ member Variables
         #TODO swaps
-        sort=[]
+        self.sort=[]
         for i in range(gv.n):
             swap = []
             swap.append(rng.randint(0,100))
             swap.append(rng.randint(0,100))
             #print (swap)
-            sort.append(swap)
+            self.sort.append(swap)
             #print (sort)
         
        
-        # fitness
-        efficiency = len(sort)      
+        # fitness 
+        self.efficiency = len(self.sort)      
         #print(efficiency)
-        effectivenes = 0            # fitness
-        age = 0                       # Number of iterations survived
+        self.effectivenes = 0            # fitness
+        self.age = 0                       # Number of iterations survived
         #TODO N2H - mutation history
-        mHistory = []
+        self.mHistory = []
         return
     
     # ~~~~~~~~~~~~~~~ member Methods
@@ -44,5 +49,67 @@ class SortIndividual:
     def spawn (self,parent2):
             # generates child (copy of iteself)
             # 2nd parent in case of cross over
-        
         return
+    
+    
+    
+    ##TODO probabilities as global variable
+    ## probabilities of each mutation occuring
+    #m1p = 0.25
+    #m2p = 0.25
+    #m3p = 0.25
+    #m4p = 0.25
+    
+    # different mutation functions
+    def add (self):
+        # add new swap tuple to sort
+        if (len(self.sort)<gV.maxSortLength):
+            newSwap = []
+            newSwap.append(rng.randint(0,100))
+            newSwap.append(rng.randint(0,100))
+            #Insert at random index 
+            self.sort.insert(rng.randint(0,len(self.sort)), newSwap)
+            #Insert mutation abrieviation
+            self.mHistory.append('A')        
+            return
+        else:
+            self.mHistory.append('~A')   
+            return
+
+    #def delete (self):
+        ## delete swap from sort
+            #self.insert(rng.randint(0,len(self)), swap)
+            #swap.append(rng.randint(0,100))
+            #swap.append(rng.randint(0,100))
+            ##Insert at random index 
+            #self.insert(rng.randint(0,len(self)), swap)
+            ##Insert mutation abrieviation
+            #self.mHistory.append('D')        
+            #return
+    #def modifiy (self):
+        ## Changes the the value of indices in a swap
+        #return
+    
+    #def mutate (self):
+    ## select mutation to be applied
+        ##Choose which mutation will occur
+        ## Parameters: 4 element list, return 1, Replace after returned, probability of each element
+        #m = np.random.choice(4,1,True,p=[m1p,m2p,m3p,m4p])
+        #if m==0:
+            ##DO m1
+            #self.add()
+        #elif m==1:
+            ##DO m2
+            #self.delete()
+        #elif m==2:
+            ##DO m3
+            #self.modifiy()
+        #else:
+            ##DO m4
+            ##DO NOTHING
+            #return
+
+##Time permitting
+    ##def swap (self):
+        ### Swap the order of two swaps in a sort
+        ##return    
