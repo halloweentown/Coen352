43dac5b1542cb9b9a6c206bf3c7a40240aaac334
Mim <miriam.lennig@gmail.com>
2018-12-12 22:13:23 -0500

    Add plot for effectiveness vs evolution iteration Add comments for variables

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 #COEN 352 Project
 #import Mutate
+import matplotlib.pyplot as plt
 import numbers as np
 import random as rng
 import SortIndividual
@@ -22,22 +23,34 @@ inputArray = gv.inputArray
 print("initial # of inversions was " + str(countInversions(inputArray)))
 
 # Create initial sort to be evolved
-parent0=SortIndividual.SortIndividual()
-parent0.applySort(inputArray)
-parent0.calcFitness()
-OGPF = parent0.effectiveness
+parent0=SortIndividual.SortIndividual()     
+parent0.applySort(inputArray)               # Apply sort to array of inputs
+parent0.calcFitness()                       # Calculate original parent fitness
+OGPF = parent0.effectiveness                # Original parent effectiveness
+nEffectiveness = []                         # List containing effectiveness scores
+nIterations = []                            # List containing iteration indices
 
-
-for i in range(4000):
+for i in range(1000):
     # Mutate Offspring of individual
+    nEffectiveness.append(parent0.effectiveness)
     child = parent0.mutate()
-    
     print("Parent effectiveness = " + str(parent0.effectiveness))
     print("Child effectiveness = " + str(child.effectiveness))
     parent0 = cull(parent0, child)
-    print(parent0.effectiveness)    
+    print(parent0.effectiveness) 
+    nIterations.append(i)
 
 print("O.G. Parent Fitness = " + str(OGPF))
+print('nEffectiveness: ', nEffectiveness)
+print('nIterations: ', nIterations)
+
+
+plt.plot(nIterations,nEffectiveness, color='g')
+#plt.plot(nIterations,nEfficiency, color='orange')
+plt.xlabel('Evolution Iteration')
+plt.ylabel('Effectiveness in Number of Inversions')
+plt.title('Fitness vs Evolution Iteration')
+plt.show()
 # End condition (# of iterations, and/or fitness criteria)
 
 
